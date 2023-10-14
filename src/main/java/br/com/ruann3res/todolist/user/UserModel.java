88b6e64d024cb397f5b782574630884c9e245ca3
Ruann3res <ruanneres@pecege.com>
2023-10-14 12:24:05 -0300

    feat: first commit

---
+++ src/main/java/br/com/ruann3res/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.ruann3res.todolist.user;
+
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String name;
+    private String username;
+    private String password;
+
+
+}
---
+++ src/test/java/br/com/ruann3res/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruann3res.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
